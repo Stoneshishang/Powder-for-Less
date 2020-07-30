729a728fbe214c2af0b2083de6ec633fd2db67e8
Larry Schirmer <larryschirmer@gmail.com>
2020-07-30 09:06:09 -0400

    add basic provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 // import Footer from "./components/Footer";
-import Header from "./components/Header";
-import ResortsSelection from "./components/ResortsSelection";
-import Dates from "./components/Dates";
-import FindTripInfoButton from "./components/FindTripInfoButton";
-import NumOfPeople from "./components/NumOfPeople";
+import Header from './components/Header';
+import ResortsSelection from './components/ResortsSelection';
+import Dates from './components/Dates';
+import FindTripInfoButton from './components/FindTripInfoButton';
+import NumOfPeople from './components/NumOfPeople';
 // import AirportInput from "./components/AirportInput";
-import AutoComplete from "./components/AutoComplete";
-import airports from "./components/airports";
-import TripInfoDetail from "./components/TripInfoDetail";
-import TripInfoSum from "./components/TripInfoSum";
+import AutoComplete from './components/AutoComplete';
+import airports from './components/airports';
+import TripInfoDetail from './components/TripInfoDetail';
+import TripInfoSum from './components/TripInfoSum';
 // import {UserContext} from './UserContext'
+import { Provider } from './ContextState';
 
 // import { v4 as uuidv4 } from "uuid";
 
@@ -33,20 +34,22 @@ const App = () => {
   // ]);
 
   return (
-    <div>
-      <Header />
-      {/* <form className='input-value-sum' value='sum' onSubmit={addInputValues}> */}
-      <ResortsSelection />
-      <label>Home Airport:</label>
-      <AutoComplete name='autocomplete-airport' items={airports} />
-      {/* <AirportInput /> */}
-      <NumOfPeople />
-      <Dates />
-      <FindTripInfoButton />
-      <TripInfoSum />
-      {/* </form> */}
-      {/* <Footer /> */}
-    </div>
+    <Provider>
+      <div>
+        <Header />
+        {/* <form className='input-value-sum' value='sum' onSubmit={addInputValues}> */}
+        <ResortsSelection />
+        <label>Home Airport:</label>
+        <AutoComplete name="autocomplete-airport" items={airports} />
+        {/* <AirportInput /> */}
+        <NumOfPeople />
+        <Dates />
+        <FindTripInfoButton />
+        <TripInfoSum />
+        {/* </form> */}
+        {/* <Footer /> */}
+      </div>
+    </Provider>
   );
 };
 
