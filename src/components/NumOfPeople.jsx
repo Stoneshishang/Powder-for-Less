43e29755a029db64985a79e9118e317e4d825e56
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-08-03 15:51:13 -0400

    finally got the Num of people passing to Sum successfully

--- src/components/NumOfPeople.jsx
+++ src/components/NumOfPeople.jsx
@@ -1,41 +1,8 @@
 import React, { useState } from "react";
 
-// function NumOfPeople() {
-//   const [num, setNum] = useState("");
-
-//   const numChangeHandler = (e) => {
-//     setNum(e.target.value);
-//     console.log(`Num is ${e.target.value}`);
-//   };
-
-//   return (
-//     <div className="passenger-number">
-//       <label htmlFor="num-people">Number of People:</label>
-//       <input
-//         name="numPeople"
-//         value={num}
-//         onChange={numChangeHandler}
-//         placeholder="Number of Passengers"
-//         // required
-//       />
-//       {console.log("Context.Consumer num is: ", num)}
-//       <pre>input Num is {JSON.stringify(num)}</pre>
-//     </div>
-//   );
-// }
-
 import { Context } from "../ContextState";
 
 function NumOfPeople() {
-  // const [num, setNum] = useState("");
-
-  // const numChangeHandler = (e) => {
-  //   const { value } = e.target;
-  //   console.log(`value is ${value}`);
-
-  //   setNum(value);
-  // };
-
   return (
     <Context.Consumer>
       {({ num, setNum }) => (
@@ -44,13 +11,12 @@ function NumOfPeople() {
           <input
             name="numPeople"
             value={num}
-            onChange={() => setNum()}
+            onChange={(event) => setNum(event.target.value)}
             placeholder="Number of Passengers"
             // required
           />
           {console.log("Context.Consumer num is: ", num)}
           <pre>input Num is {JSON.stringify(num)}</pre>
-          <button onClick={setNum}>Submit NumOfPeople</button>
         </div>
       )}
     </Context.Consumer>
