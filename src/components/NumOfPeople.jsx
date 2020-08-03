dcba026e48c242d854755b262a34c009fdd8fdf0
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-08-03 15:04:23 -0400

    Num of People useState and context consumer isn't working.

--- src/components/NumOfPeople.jsx
+++ src/components/NumOfPeople.jsx
@@ -1,60 +1,60 @@
 import React, { useState } from "react";
 
-function NumOfPeople() {
-  const [num, setNum] = useState("");
-
-  const numChangeHandler = (e) => {
-    setNum(e.target.value);
-    console.log(`Num is ${e.target.value}`);
-  };
-
-  return (
-    <div className="passenger-number">
-      <label htmlFor="num-people">Number of People:</label>
-      <input
-        name="numPeople"
-        value={num}
-        onChange={numChangeHandler}
-        placeholder="Number of Passengers"
-        // required
-      />
-      {console.log("Context.Consumer num is: ", num)}
-      <pre>input Num is {JSON.stringify(num)}</pre>
-    </div>
-  );
-}
-
-// import { Context } from "../ContextState";
-
 // function NumOfPeople() {
-//   // const [num, setNum] = useState("");
+//   const [num, setNum] = useState("");
 
-//   // const numChangeHandler = (e) => {
-//   //   const { value } = e.target;
-//   //   console.log(`value is ${value}`);
-
-//   //   setNum(value);
-//   // };
+//   const numChangeHandler = (e) => {
+//     setNum(e.target.value);
+//     console.log(`Num is ${e.target.value}`);
+//   };
 
 //   return (
-//     <Context.Consumer>
-//       {({ num, setNum }) => (
-//         <div className="passenger-number">
-//           <label htmlFor="num-people">Number of People:</label>
-//           <input
-//             name="numPeople"
-//             value={num}
-//             onChange={() => setNum()}
-//             placeholder="Number of Passengers"
-//             // required
-//           />
-//           {console.log("Context.Consumer num is: ", num)}
-//           <pre>input Num is {JSON.stringify(num)}</pre>
-//           <button onClick={setNum}>Submit NumOfPeople</button>
-//         </div>
-//       )}
-//     </Context.Consumer>
+//     <div className="passenger-number">
+//       <label htmlFor="num-people">Number of People:</label>
+//       <input
+//         name="numPeople"
+//         value={num}
+//         onChange={numChangeHandler}
+//         placeholder="Number of Passengers"
+//         // required
+//       />
+//       {console.log("Context.Consumer num is: ", num)}
+//       <pre>input Num is {JSON.stringify(num)}</pre>
+//     </div>
 //   );
 // }
+
+import { Context } from "../ContextState";
+
+function NumOfPeople() {
+  // const [num, setNum] = useState("");
+
+  // const numChangeHandler = (e) => {
+  //   const { value } = e.target;
+  //   console.log(`value is ${value}`);
+
+  //   setNum(value);
+  // };
+
+  return (
+    <Context.Consumer>
+      {({ num, setNum }) => (
+        <div className="passenger-number">
+          <label htmlFor="num-people">Number of People:</label>
+          <input
+            name="numPeople"
+            value={num}
+            onChange={() => setNum()}
+            placeholder="Number of Passengers"
+            // required
+          />
+          {console.log("Context.Consumer num is: ", num)}
+          <pre>input Num is {JSON.stringify(num)}</pre>
+          <button onClick={setNum}>Submit NumOfPeople</button>
+        </div>
+      )}
+    </Context.Consumer>
+  );
+}
 
 export default NumOfPeople;
