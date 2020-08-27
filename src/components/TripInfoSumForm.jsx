ecc868149483e4a755dd9348a0b9ec23060d0190
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-08-27 17:39:10 -0400

    dynamic table feature added, ready for deployment

--- src/components/TripInfoSumForm.jsx
+++ src/components/TripInfoSumForm.jsx
@@ -1,9 +1,5 @@
 import React, { useContext, useEffect, useState, useRef } from "react";
-<<<<<<< HEAD
-// import _ from "lodash";
-=======
 import SumTable from "./SumTable";
->>>>>>> fetch-both-API-at-once
 import { Context } from "../ContextState";
 import _ from "lodash";
 
@@ -16,59 +12,6 @@ const TripInfoSum = () => {
     // num,
     departureDate,
     returnDate,
-<<<<<<< HEAD
-    weatherData,
-    fetchWeatherData,
-    flightData,
-    fetchFlightData,
-  } = useContext(Context);
-
-  const [count, setCount] = useState(0);
-  const [countEffect, setCountEffect] = useState(0);
-  const weatherResultArray = useRef([]);
-  const flightReasultArray = useRef([]);
-
-  // update selected date item
-  useEffect(() => {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-    const yyyy = today.getFullYear();
-    const currentDate = yyyy + "-" + mm + "-" + dd;
-    const departureTimeDiff = new Date(departureDate) - new Date(currentDate);
-    const daysDepartureTimeDiff = Number(
-      departureTimeDiff / (1000 * 60 * 60 * 24),
-    );
-    // fetch the chosen day's weather conditon.
-    let item = weatherData.daily;
-    let latCords = weatherData.lat;
-    if (item !== undefined) {
-      item = weatherData.daily[daysDepartureTimeDiff].humidity;
-      weatherResultArray.current.push(item);
-    }
-    setCountEffect(countEffect + 1);
-    console.log(
-      `---------useEffect triggered ${countEffect} times-------------`,
-    );
-
-    console.log(
-      `1. chosen resort latitude ${latCords}, humidity for testing is: ${JSON.stringify(
-        item,
-      )}`,
-    );
-    // console.log("departure time diff is: ", daysDepartureTimeDiff);
-    let flightDest = flightData.Places;
-    let flightPrice = flightData.Quotes;
-
-    flightReasultArray.current.push(flightPrice);
-    // if (flightDest !== undefined && flightPrice !== undefined) {
-    // flightDest = flightData.Places
-
-    console.log("2. flight Destination is: ", flightDest);
-    console.log("3. flight Price is: ", flightPrice);
-    // }
-  }, [weatherData, flightData]);
-=======
 
     bothData,
     fetchBothData,
@@ -85,16 +28,11 @@ const TripInfoSum = () => {
 
   const chosenResortsObjArr = selectedMW.concat(selectedRockies);
   // console.log("   choseResortsObjArr is: ", chosenResortsObjArr);
->>>>>>> fetch-both-API-at-once
 
   //fetch weather data base on the resorts' cordinates.
   const chosenResortsCordsArr = selectedMW
     .concat(selectedRockies)
     .map((x) => x.value);
-
-  console.log(`chosenResortsCordsArr is: `, chosenResortsCordsArr);
-  console.log("weatherResultArr outside is: ", weatherResultArray);
-  console.log("flightReasultArray outside is: ", flightReasultArray);
   //fetch flight data base on resorts' selections.
   const chosenResortsAirportArr = selectedMW
     .concat(selectedRockies)
@@ -111,17 +49,6 @@ const TripInfoSum = () => {
     airport.length - 4,
     airport.length - 1,
   );
-
-  // console.log("latCords is: ", latCords);
-
-  // const latArr = chosenResortsCordsArr.map((x) => _.round(x.lat, 2)); // use lodash to round it.
-
-  // console.log("latArr is: ", latArr);
-
-  // //storing API results for display.
-  // const identifyResorts = latArr.indexOf(latCords);
-
-  // console.log("identifyResorts is: ", identifyResorts);
 
   //find the unique items in the string.
   // const uniqueAirports = [...new Set(chosenResortsAirportArr)];
@@ -165,7 +92,7 @@ const TripInfoSum = () => {
         return s + (v || 0);
       }, 0);
 
-      console.log("   tripSnowSum is: ", tripSnowSum);
+      // console.log("   tripSnowSum is: ", tripSnowSum);
 
       //Match response Data with corresponding resort
       const dataIDObj = chosenResortsObjArr.filter((obj) => {
@@ -209,16 +136,11 @@ const TripInfoSum = () => {
 
     for (let i = 0; i < tripObjectsArr.length; i++) {
       const chosenResortsCords = tripObjectsArr[i].ResortCords;
-<<<<<<< HEAD
-      const lat = chosenResortsCords.lat;
-      const lon = chosenResortsCords.lon;
-=======
       // console.log("chosenResortCords is: ", chosenResortsCords);
 
       const lat = chosenResortsCords.lat;
       const lon = chosenResortsCords.lon;
 
->>>>>>> fetch-both-API-at-once
       //fetch flight data base on the user location, selected resorts, and dates.
       const originplace = homeAirportCode;
       const destinationplace = tripObjectsArr[i].Airport;
@@ -226,29 +148,6 @@ const TripInfoSum = () => {
       const outboundpartialdate = departureDate;
       const inboundpartialdate = returnDate;
 
-<<<<<<< HEAD
-      fetchWeatherData({ lat, lon });
-      fetchFlightData({
-        originplace,
-        destinationplace,
-        outboundpartialdate,
-        inboundpartialdate,
-      });
-
-      // fetchWeatherData({ lat, lon });
-      // if (originplace !== destinationplace) {
-      //   fetchFlightData({
-      //     originplace,
-      //     destinationplace,
-      //     outboundpartialdate,
-      //     inboundpartialdate,
-      //   });
-      // } else {
-      //   console.log(
-      //     "You live very close to your selected resorts, you could drive!",
-      //   );
-      // }
-=======
       if (originplace !== destinationplace) {
         fetchBothData({
           lat,
@@ -261,7 +160,6 @@ const TripInfoSum = () => {
       } else {
         console.log("   Drive is better!");
       }
->>>>>>> fetch-both-API-at-once
     }
   };
 
