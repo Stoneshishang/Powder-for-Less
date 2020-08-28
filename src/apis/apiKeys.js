6b2ee89833abed588af98f20166bdbf8fb744a90
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-08-28 18:30:59 -0400

    start ignoring apiKeys.js

--- src/apis/apiKeys.js
+++
@@ -1,9 +0,0 @@
-// this should be stored in a .env file
-// instead of being commited as a string
-// https://stackoverflow.com/questions/48699820/how-do-i-hide-api-key-in-create-react-app
-const weatherAPIkey = "7627edc2f6ba9856d14e74e740f35ff0";
-
-//flight API key from rapid API
-const flightAPIkey = "0f6322963cmshc2c24de3b656c85p184e30jsnbbb318965fe8";
-
-export { weatherAPIkey, flightAPIkey };
